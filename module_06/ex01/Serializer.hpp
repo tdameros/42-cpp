ed571319288d03d45d98b651fc387d6c8e397a10
tdameros <tomdamerose@gmail.com>
2023-09-28 22:51:49 +0200

    module06 ex01: change canonical form to private scope

--- module_06/ex01/Serializer.hpp
+++ module_06/ex01/Serializer.hpp
@@ -8,14 +8,16 @@
 class Serializer
 {
 public:
+
+	static uintptr_t	serialize(Data* ptr);
+	static Data*		deserialize(uintptr_t raw);
+
+private:
 	Serializer(void);
 	Serializer(const Serializer& other);
 	~Serializer(void);
 
 	Serializer &operator=(const Serializer& other);
-
-	static uintptr_t	serialize(Data* ptr);
-	static Data*		deserialize(uintptr_t raw);
 };
 
 #endif
